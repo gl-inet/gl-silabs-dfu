03463ce65f516ee84f8475a68eeaa6511423268d
lancersky2017 <luoyejiang@gl-inet.com>
2022-06-10 18:12:30 +0800

    improve rcp version check

--- src/xmodem/xmodem_main.c
+++ src/xmodem/xmodem_main.c
@@ -22,6 +22,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "gl_hal.h"
@@ -45,16 +47,58 @@ char TURN_ON_DFU_ENABLE[64];
 char TURN_OFF_DFU_ENABLE[64];
 int g_verbose = 0;
 int ver_check = 0;
+char g_filepath[256] = { 0 };
 
 char new_ot_efr32_commit_hash[16] = { 0 };
 
 uint8_t spinel_get_module_version_cmd[7] = { 0x7e, 0x82, 0x02, 0x02, 0x3a, 0x6f, 0x7e };
 uint8_t spinel_get_module_version_rsp[128] = { 0 };
 
-static int get_new_firmware_commit_hash(const char* file_path, char* commit_hash);
-static int check_firmware_version(char* old_firmware_version, char* new_firmware_hash);
+// static int get_new_firmware_commit_hash(const char* file_path, char* commit_hash);
+// static int check_firmware_version(char* old_firmware_version, char* new_firmware_hash);
 
 static dfu_step_e step;
+
+uint64_t get_rcp_timestamp(char* rcp_version)
+{
+    char* end_str;
+    char* str = strtok_r(rcp_version, ";", &end_str);
+    str = strtok_r(NULL, ";", &end_str);
+    str = strtok_r(NULL, ";", &end_str);
+
+    struct tm timeinfo;
+    strptime(str, " %b %d %Y %H:%M:%S", &timeinfo);
+
+    timeinfo.tm_sec = 0;
+    timeinfo.tm_min = 0;
+    timeinfo.tm_hour = 0;
+
+    time_t timestamp;
+    timestamp = mktime(&timeinfo);
+    if (g_verbose)
+        printf("Current rcp timestamp: %ld\n", timestamp);
+
+    return timestamp;
+}
+
+uint64_t get_file_timestamp(char* filepath)
+{
+    struct stat st;
+    if (stat(filepath, &st) != 0) {
+        return 0;
+    }
+
+    char* ret = strrchr(filepath, '-');
+    char* end_str;
+    char* str = strtok_r(ret + 1, ".", &end_str);
+
+    time_t timestamp;
+    timestamp = atol(str);
+    if (g_verbose)
+        printf("New rcp timestamp: %ld\n", timestamp);
+
+    return timestamp;
+}
 
 int dfu_process(uint8_t* out_file, uint32_t file_size)
 {
@@ -83,10 +127,15 @@ int dfu_process(uint8_t* out_file, uint32_t file_size)
                     goto start_dfu;
                     break;
                 }
-                if (0 == check_firmware_version(&spinel_get_module_version_rsp[offset + 4], new_ot_efr32_commit_hash)) {
-                    printf("New firmware version is same as ole firmware. DFU end!\n");
+                if (get_rcp_timestamp(spinel_get_module_version_rsp) >= get_file_timestamp(g_filepath)) {
+                    printf("No need to upgrade. DFU end!\n");
                     return 1;
                 }
+                // printf("spinel_get_module_version_rsp=%s\n", spinel_get_module_version_rsp);
+                // if (0 == check_firmware_version(&spinel_get_module_version_rsp[offset + 4], new_ot_efr32_commit_hash)) {
+                //     printf("New firmware version is same as ole firmware. DFU end!\n");
+                //     return 1;
+                // }
             } else {
                 printf("Get version response failed!\n");
             }
@@ -183,10 +232,10 @@ int xmodem_main(int argc, char* argv[])
 
     if ((argv[7] != NULL && !strncmp(argv[7], "-c", 2)) || (argv[6] != NULL && !strncmp(argv[6], "-c", 2))) {
         ver_check = 1; // only for openthread module firmware build from GL-inet
-        if (0 != get_new_firmware_commit_hash(argv[2], new_ot_efr32_commit_hash)) {
-            printf("Get new firmware openthread commit hash failed!\n");
-            return -1;
-        }
+        // if (0 != get_new_firmware_commit_hash(argv[2], new_ot_efr32_commit_hash)) {
+        //     printf("Get new firmware openthread commit hash failed!\n");
+        //     return -1;
+        // }
     }
 
 #if 0
@@ -207,6 +256,7 @@ int xmodem_main(int argc, char* argv[])
         printf("not find file\r\n");
         return -1;
     }
+    strcpy(g_filepath, argv[2]);
 
     uint8_t buffer[1024 * 1024];
     size_t read_size;
@@ -216,7 +266,7 @@ int xmodem_main(int argc, char* argv[])
 
     strcpy(TTY_NUM, argv[3]);
     if (ver_check) {
-        ret = hal_init(TTY_NUM, 460800, 1);
+        ret = hal_init(TTY_NUM, 1000000, 1);
         step = VERSION_CHECK;
     } else {
         ret = hal_init(TTY_NUM, 115200, 0);
@@ -241,73 +291,73 @@ int xmodem_main(int argc, char* argv[])
     return 0;
 }
 
-static int get_new_firmware_commit_hash(const char* file_path, char* commit_hash)
-{
-    char tmp_path[128] = { 0 };
-    strcpy(tmp_path, file_path);
+// static int get_new_firmware_commit_hash(const char* file_path, char* commit_hash)
+// {
+//     char tmp_path[128] = { 0 };
+//     strcpy(tmp_path, file_path);
 
-    const char str[2] = "/";
-    char file_name[64] = { 0 };
-    char* tmp_str = NULL;
-    tmp_str = strtok(tmp_path, str);
-    while (NULL != tmp_str) {
-        strcpy(file_name, tmp_str);
-        tmp_str = strtok(NULL, str);
-    }
-    // printf("Get file name: %s\n", file_name);
+//     const char str[2] = "/";
+//     char file_name[64] = { 0 };
+//     char* tmp_str = NULL;
+//     tmp_str = strtok(tmp_path, str);
+//     while (NULL != tmp_str) {
+//         strcpy(file_name, tmp_str);
+//         tmp_str = strtok(NULL, str);
+//     }
+//     // printf("Get file name: %s\n", file_name);
 
-    const char str1[2] = "-";
-    char openthread_commit_str[32] = { 0 };
-    tmp_str = strtok(file_name, str1);
-    while (NULL != tmp_str) {
-        strcpy(openthread_commit_str, tmp_str);
-        tmp_str = strtok(NULL, str1);
-    }
-    // printf("Get openthread & ot-efr32 commit hash: %s\n", openthread_commit_str);
+//     const char str1[2] = "-";
+//     char openthread_commit_str[32] = { 0 };
+//     tmp_str = strtok(file_name, str1);
+//     while (NULL != tmp_str) {
+//         strcpy(openthread_commit_str, tmp_str);
+//         tmp_str = strtok(NULL, str1);
+//     }
+//     // printf("Get openthread & ot-efr32 commit hash: %s\n", openthread_commit_str);
 
-    const char str2[2] = "_";
-    char ot_efr32_commit_str[16] = { 0 };
-    tmp_str = strtok(openthread_commit_str, str2);
-    if (tmp_str != NULL) {
-        strcpy(ot_efr32_commit_str, tmp_str);
-    }
-    printf("Get ot-efr32 commit hash: %s\n", ot_efr32_commit_str);
+//     const char str2[2] = "_";
+//     char ot_efr32_commit_str[16] = { 0 };
+//     tmp_str = strtok(openthread_commit_str, str2);
+//     if (tmp_str != NULL) {
+//         strcpy(ot_efr32_commit_str, tmp_str);
+//     }
+//     printf("Get ot-efr32 commit hash: %s\n", ot_efr32_commit_str);
 
-    int hash_len = strlen(ot_efr32_commit_str);
-    if (hash_len == 7) {
-        strncpy(commit_hash, ot_efr32_commit_str, hash_len);
-    } else {
-        return -1;
-    }
+//     int hash_len = strlen(ot_efr32_commit_str);
+//     if (hash_len == 7) {
+//         strncpy(commit_hash, ot_efr32_commit_str, hash_len);
+//     } else {
+//         return -1;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
-static int check_firmware_version(char* old_firmware_version, char* new_firmware_hash)
-{
-    char tmp_version[128] = { 0 };
-    strcpy(tmp_version, old_firmware_version);
+// static int check_firmware_version(char* old_firmware_version, char* new_firmware_hash)
+// {
+//     char tmp_version[128] = { 0 };
+//     strcpy(tmp_version, old_firmware_version);
 
-    const char str[2] = ";";
-    char ot_version[64] = { 0 };
-    char* tmp_str = NULL;
-    tmp_str = strtok(tmp_version, str);
-    strcpy(ot_version, tmp_str);
-    // printf("Get old firmware ot version: %s\n", ot_version);
+//     const char str[2] = ";";
+//     char ot_version[64] = { 0 };
+//     char* tmp_str = NULL;
+//     tmp_str = strtok(tmp_version, str);
+//     strcpy(ot_version, tmp_str);
+//     // printf("Get old firmware ot version: %s\n", ot_version);
 
-    const char str1[2] = "-";
-    char ot_efr32_commit_hash[16] = { 0 };
-    tmp_str = NULL;
-    tmp_str = strtok(ot_version, str1);
-    while (NULL != tmp_str) {
-        strcpy(ot_efr32_commit_hash, tmp_str);
-        tmp_str = strtok(NULL, str1);
-    }
-    printf("Get old firmware ot commit hash: %s\n", ot_efr32_commit_hash);
-    if (7 != strlen(ot_efr32_commit_hash)) {
-        printf("Old firmware ot commit hash len failed!\n");
-        return -1;
-    }
+//     const char str1[2] = "-";
+//     char ot_efr32_commit_hash[16] = { 0 };
+//     tmp_str = NULL;
+//     tmp_str = strtok(ot_version, str1);
+//     while (NULL != tmp_str) {
+//         strcpy(ot_efr32_commit_hash, tmp_str);
+//         tmp_str = strtok(NULL, str1);
+//     }
+//     printf("Get old firmware ot commit hash: %s\n", ot_efr32_commit_hash);
+//     if (7 != strlen(ot_efr32_commit_hash)) {
+//         printf("Old firmware ot commit hash len failed!\n");
+//         return -1;
+//     }
 
-    return strncmp(ot_efr32_commit_hash, new_firmware_hash, 7);
-}
+//     return strncmp(ot_efr32_commit_hash, new_firmware_hash, 7);
+// }
